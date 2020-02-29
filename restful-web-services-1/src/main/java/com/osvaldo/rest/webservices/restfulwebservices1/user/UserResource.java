0af2a6552a906b30af88b97578a81678b68f1fc8
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2020-02-29 15:03:57 -0500

    Created the UserNotFoundException, and now the request shows the path from a new user created

--- restful-web-services-1/src/main/java/com/osvaldo/rest/webservices/restfulwebservices1/user/UserResource.java
+++ restful-web-services-1/src/main/java/com/osvaldo/rest/webservices/restfulwebservices1/user/UserResource.java
@@ -1,9 +1,11 @@
 package com.osvaldo.rest.webservices.restfulwebservices1.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,7 +23,23 @@ public class UserResource {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if (user==null)
+            throw new UserNotFoundException("id-"+ id);
+
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
