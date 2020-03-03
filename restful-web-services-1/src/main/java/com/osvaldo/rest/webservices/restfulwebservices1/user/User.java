a86178dc0531b8710a0b7840ca8a6c026221a0a4
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2020-03-02 19:57:19 -0500

    Modified the Customized Response and the User and User Resource to display a certain type of message for a certain type of scenario

--- restful-web-services-1/src/main/java/com/osvaldo/rest/webservices/restfulwebservices1/user/User.java
+++ restful-web-services-1/src/main/java/com/osvaldo/rest/webservices/restfulwebservices1/user/User.java
@@ -1,12 +1,16 @@
 package com.osvaldo.rest.webservices.restfulwebservices1.user;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 public class User {
     private Integer id;
 
+    @Size(min=2, message = "Name should have at least two characters")
     private String name;
 
+    @Past
     private Date birthDate;
 
     protected User() {
--- restful-web-services-1/src/main/java/com/osvaldo/rest/webservices/restfulwebservices1/user/UserResource.java
+++ restful-web-services-1/src/main/java/com/osvaldo/rest/webservices/restfulwebservices1/user/UserResource.java
@@ -1,10 +1,15 @@
 package com.osvaldo.rest.webservices.restfulwebservices1.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -27,20 +32,24 @@ public class UserResource {
         if (user==null)
             throw new UserNotFoundException("id-"+ id);
 
+        EntityModel<User> model = new EntityModel<>(user);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        model.add(linkTo.withRel("all-users"));
+
+
         return user;
     }
 
     @DeleteMapping("/users/{id}")
-    public User deleteUser(@PathVariable int id) {
-        User user = service.findOne(id);
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
         if (user==null)
             throw new UserNotFoundException("id-"+ id);
 
-        return user;
     }
 
     @PostMapping("/users")
-    public ResponseEntity<Object> createUser(@RequestBody User user) {
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
 
         URI location = ServletUriComponentsBuilder
