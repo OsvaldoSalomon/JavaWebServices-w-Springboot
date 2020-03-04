89994327f13affcd7fff61263e5699b4aba4a724
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2020-03-03 19:53:06 -0500

    Created the message.properties and the message fr properties, so far they are not working the video is using an older version or something cause I have the same code and it's just not working

--- restful-web-services-1/src/main/java/com/osvaldo/rest/webservices/restfulwebservices1/RestfulWebServices1Application.java
+++ restful-web-services-1/src/main/java/com/osvaldo/rest/webservices/restfulwebservices1/RestfulWebServices1Application.java
@@ -2,12 +2,34 @@ package com.osvaldo.rest.webservices.restfulwebservices1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServices1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServices1Application.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 
 }
--- restful-web-services-1/src/main/java/com/osvaldo/rest/webservices/restfulwebservices1/helloworld/HelloWorldController.java
+++ restful-web-services-1/src/main/java/com/osvaldo/rest/webservices/restfulwebservices1/helloworld/HelloWorldController.java
@@ -1,6 +1,11 @@
 package com.osvaldo.rest.webservices.restfulwebservices1.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 //Controller
 @RestController
@@ -9,6 +14,10 @@ public class HelloWorldController {
     //GET
     //URI
     //method - "Hello World"
+
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
@@ -23,6 +32,12 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        return messageSource.getMessage("good.morning.message", null,
+                LocaleContextHolder.getLocale());
     }
 
 }
