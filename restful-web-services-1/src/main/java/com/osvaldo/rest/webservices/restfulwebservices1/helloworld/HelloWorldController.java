cc5d8f073e8718652249531b65f97d72c1c4c9da
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2020-03-06 05:49:31 -0500

    Finished the restful-web-services, added some files to the versioning folder, part of this commit is also from yesterday

--- restful-web-services-1/src/main/java/com/osvaldo/rest/webservices/restfulwebservices1/helloworld/HelloWorldController.java
+++ restful-web-services-1/src/main/java/com/osvaldo/rest/webservices/restfulwebservices1/helloworld/HelloWorldController.java
@@ -5,8 +5,6 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Locale;
-
 //Controller
 @RestController
 public class HelloWorldController {
